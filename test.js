2637ec73b643487b719bdecc4377a3924514cf9e
Gabriel Izaias <gabriel.izaias@gmail.com>
2019-07-08 17:04:26 +0200

    Update execa devDependency to 2.0.3
    Also update tests to new syntax

--- test.js
+++ test.js
@@ -2,30 +2,30 @@ import test from 'ava'
 import execa from 'execa'
 
 test('it generates a valid CNPJ', async t => {
-  const generated = await execa.stdout('./index.js')
+  const generated = (await execa('./index.js')).stdout
   t.true(generated.length > 0)
 
-  const validated = await execa.stdout('./index.js', ['-v', generated])
+  const validated = (await execa('./index.js', ['-v', generated])).stdout
   t.true(validated === 'true')
 })
 
 test('it generates a valid numbers-only CNPJ', async t => {
-  const generated = await execa.stdout('./index.js', ['-n'])
+  const generated = (await execa('./index.js', ['-n'])).stdout
   t.false(/\.|-|\//.test(generated))
 
-  const validated = await execa.stdout('./index.js', ['-v', generated])
+  const validated = (await execa('./index.js', ['-v', generated])).stdout
   t.true(validated === 'true')
 })
 
 test('it validates a given CNPJ', async t => {
-  const valid = await execa.stdout('./index.js', ['-v', '29.709.817/0001-15'])
+  const valid = (await execa('./index.js', ['-v', '29.709.817/0001-15'])).stdout
   t.true(valid === 'true')
 
-  const invalid = await execa.stdout('./index.js', ['-v', '29.709.817/0001-00'])
+  const invalid = (await execa('./index.js', ['-v', '29.709.817/0001-00'])).stdout
   t.true(invalid === 'false')
 })
 
 test('it formats a given CNPJ', async t => {
-  const formatted = await execa.stdout('./index.js', ['-f', '29709817000115'])
+  const formatted = (await execa('./index.js', ['-f', '29709817000115'])).stdout
   t.is(formatted, '29.709.817/0001-15')
 })
